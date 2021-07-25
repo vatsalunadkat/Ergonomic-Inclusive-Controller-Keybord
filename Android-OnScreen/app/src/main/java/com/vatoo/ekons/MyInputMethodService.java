1571d9cf0fb86f4f53d67540d41058d21d7a521b
Vatsal Unadkat <vatoobhai@gmail.com>
2021-07-25 12:41:12 +0530

    Working custom keyboard (only numbers)

---
+++ Android-OnScreen/app/src/main/java/com/vatoo/ekons/MyInputMethodService.java
@@ -0,0 +1,64 @@
+package com.vatoo.ekons;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    @Override
+    public View onCreateInputView() {
+        // get the KeyboardView and add our Keyboard layout to it
+        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.number_pad);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+
+        InputConnection ic = getCurrentInputConnection();
+        if (ic == null) return;
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                CharSequence selectedText = ic.getSelectedText(0);
+                if (TextUtils.isEmpty(selectedText)) {
+                    // no selection, so delete previous character
+                    ic.deleteSurroundingText(1, 0);
+                } else {
+                    // delete the selection
+                    ic.commitText("", 1);
+                }
+                break;
+            default:
+                char code = (char) primaryCode;
+                ic.commitText(String.valueOf(code), 1);
+        }
+    }
+
+    @Override
+    public void onPress(int primaryCode) { }
+
+    @Override
+    public void onRelease(int primaryCode) { }
+
+    @Override
+    public void onText(CharSequence text) { }
+
+    @Override
+    public void swipeLeft() { }
+
+    @Override
+    public void swipeRight() { }
+
+    @Override
+    public void swipeDown() { }
+
+    @Override
+    public void swipeUp() { }
+}
